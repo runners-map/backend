143af67050ee47f905931f77584330074298f69b
jeonjiwon <jiwon9609@naver.com>
2024-09-29 17:46:39 +0900

    init commit

---
+++ src/main/java/com/project/service_runners_map/ServiceRunnersMapApplication.java
@@ -0,0 +1,13 @@
+package com.project.service_runners_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRunnersMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRunnersMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/service_runners_map/ServiceRunnersMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.service_runners_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRunnersMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
