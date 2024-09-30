d37af5260c98143aea56a41487738e188d57144e
lauren5141935 <lauren5141935@gmail.com>
2024-10-01 00:02:19 +0900

    feat: UserRepository

--- src/main/java/com/service/runnersmap/repository/UserRepository.java
+++ src/main/java/com/service/runnersmap/repository/UserRepository.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-  Optional<User> findByUserId(UUID userId) ;
-
+   Optional<User> findByUserId(UUID userId) ;
    User findByProviderId(String providerId);
 
 }
