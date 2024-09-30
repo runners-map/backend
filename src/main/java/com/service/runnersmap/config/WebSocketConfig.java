853b395edcdb0e6241afb273559a8d91e81d1dc4
HueJo <slack_water@naver.com>
2024-10-01 00:12:26 +0900

    Refactor : 엔드포인트 수정

--- src/main/java/com/service/runnersmap/config/WebSocketConfig.java
+++ src/main/java/com/service/runnersmap/config/WebSocketConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker   // stomp 사용을 위해 선언
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   // WebSocketConfigurer : 1대1 채팅방
   // WebSocketMessageBrokerConfigurer : 단톡방 - 메시징 시스템 필요
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws/chat")  // 클라이언트가 WebSocket 연결을 위해 사용할 STOMP 엔드포인트(URL)
+    registry.addEndpoint("/ws-stomp")  // 클라이언트가 WebSocket 연결을 위해 사용할 STOMP 엔드포인트(URL)
         .setAllowedOriginPatterns("*")  // 일단은 모든 출처에서 연결 허용 (추후 수정 해야 할 듯)
         .withSockJS();
   }
@@ -22,8 +22,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
 
-    registry.enableSimpleBroker("/topic");  // (서버 -> 클라이언트) 메시지를 전송할 경로
-    registry.setApplicationDestinationPrefixes("/app");    // (클라이언트 -> 서버) 메시지를 전송할 경로
+    registry.enableSimpleBroker("/sub");  // (서버 -> 클라이언트) 메시지를 전송할 경로
+    registry.setApplicationDestinationPrefixes("/pub");    // (클라이언트 -> 서버) 메시지를 전송할 경로
   }
 
 
