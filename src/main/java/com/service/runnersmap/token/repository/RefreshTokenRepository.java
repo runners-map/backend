50058cd16efa3f924a6960c6835eaf6438d26c69
lauren5141935 <lauren5141935@gmail.com>
2024-10-02 00:36:46 +0900

    feat: RefreshToken & RefreshTokenRepository

---
+++ src/main/java/com/service/runnersmap/token/repository/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.service.runnersmap.token.repository;
+
+import com.nimbusds.oauth2.sdk.token.RefreshToken;
+import jakarta.transaction.Transactional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+  RefreshToken findByUserId(UUID userId);
+
+  @Transactional
+  @Modifying
+  void deleteByUserId(UUID userId);
+}
