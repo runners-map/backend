a319426fe729db28b8cefb5b1e1db75763f92710
lauren5141935 <lauren5141935@gmail.com>
2024-10-02 12:50:48 +0900

    feat: oauth 관련 handler(성공 및 실패 여부)

---
+++ src/main/java/com/service/runnersmap/auth/handler/OAuthLoginFailureHandler.java
@@ -0,0 +1,24 @@
+package com.service.runnersmap.auth.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuthLoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+  @Override
+  public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+    log.error("LOGIN FAILED : {}", exception.getMessage());
+    super.onAuthenticationFailure(request, response, exception);
+
+  }
+}
