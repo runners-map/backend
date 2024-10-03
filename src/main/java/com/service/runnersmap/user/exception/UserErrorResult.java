688600e7bf96b30042a83da8364d9f4863721652
lauren5141935 <lauren5141935@gmail.com>
2024-10-03 18:27:19 +0900

    feat: user 에러처리 기능 구현

---
+++ src/main/java/com/service/runnersmap/user/exception/UserErrorResult.java
@@ -0,0 +1,36 @@
+package com.service.runnersmap.user.exception;
+
+import com.service.runnersmap.common.code.BaseErrorCode;
+import com.service.runnersmap.common.dto.ErrorReasonDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorResult implements BaseErrorCode {
+  NOT_FOUND_USER(HttpStatus.NOT_FOUND, "404", "존재하지 않는 유저입니다.");
+
+  private final HttpStatus httpStatus;
+  private final String code;
+  private final String message;
+
+  @Override
+  public ErrorReasonDto getReason() {
+    return ErrorReasonDto.builder()
+        .isSuccess(false)
+        .code(code)
+        .message(message)
+        .build();
+  }
+
+  @Override
+  public ErrorReasonDto getReasonHttpStatus() {
+    return ErrorReasonDto.builder()
+        .isSuccess(false)
+        .httpStatus(httpStatus)
+        .code(code)
+        .message(message)
+        .build();
+  }
+}
