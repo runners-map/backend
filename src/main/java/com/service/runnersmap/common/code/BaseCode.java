5e6bc1137ae869b3221e4b03cfed0280ebd6506f
lauren5141935 <lauren5141935@gmail.com>
2024-10-03 17:57:02 +0900

    feat: BaseCode 추가

---
+++ src/main/java/com/service/runnersmap/common/code/BaseCode.java
@@ -0,0 +1,10 @@
+package com.service.runnersmap.common.code;
+
+import com.service.runnersmap.common.dto.ReasonDto;
+
+public interface BaseCode {
+  public ReasonDto getReason();
+
+  public ReasonDto getReasonHttpStatus();
+
+}
