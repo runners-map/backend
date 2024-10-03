90d5ad1dfa46f149971d86e92050134d132fbb84
lauren5141935 <lauren5141935@gmail.com>
2024-10-03 19:45:47 +0900

    feat: user error 처리 기능 구현

--- src/main/java/com/service/runnersmap/exception/GlobalExceptionHandler.java
+++ src/main/java/com/service/runnersmap/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.service.runnersmap.common.ApiResponse;
 import com.service.runnersmap.common.code.BaseErrorCode;
 import com.service.runnersmap.token.exception.TokenErrorResult;
 import com.service.runnersmap.token.exception.TokenException;
+import com.service.runnersmap.user.exception.UserErrorResult;
+import com.service.runnersmap.user.exception.UserException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +19,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(TokenException.class)
   public ResponseEntity<ApiResponse<BaseErrorCode>> handleTokenException(TokenException e) {
     TokenErrorResult errorResult = e.getTokenErrorResult();
+    return ApiResponse.onFailure(errorResult);
+  }
+  @ExceptionHandler(UserException.class)
+  public ResponseEntity<ApiResponse<BaseErrorCode>> handleUserException(UserException e) {
+    UserErrorResult errorResult = e.getUserErrorResult();
     return ApiResponse.onFailure(errorResult);
   }
 }
