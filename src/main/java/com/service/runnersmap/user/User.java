9472cecbb5a4e63609abe16a639e573f23e95666
lauren5141935 <lauren5141935@gmail.com>
2024-10-04 19:45:57 +0900

    fix: User 수정

--- src/main/java/com/service/runnersmap/user/User.java
+++ src/main/java/com/service/runnersmap/user/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -36,5 +37,47 @@ public class User extends BaseEntity {
   @UpdateTimestamp
   @Column(name = "updated_at", length = 20)
   private LocalDateTime updatedAt;
+
+  private User(UserBuilder builder) {
+    this.userId = builder.userId;
+    this.name = builder.name;
+    this.provider = builder.provider;
+    this.providerId = builder.providerId;
+  }
+
+  public static class UserBuilder {
+    private UUID userId;  // userId 필드 추가
+    private String name;
+    private String provider;
+    private String providerId;
+
+    public UserBuilder userId(UUID userId) {
+      this.userId = userId;  // userId 메소드 구현
+      return this;
+    }
+
+    public UserBuilder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public UserBuilder provider(String provider) {
+      this.provider = provider;
+      return this;
+    }
+
+    public UserBuilder providerId(String providerId) {
+      this.providerId = providerId;
+      return this;
+    }
+
+    public User build() {
+      return new User(this);
+    }
+  }
+
+  public static UserBuilder builder() {
+    return new UserBuilder();
+  }
 
 }
