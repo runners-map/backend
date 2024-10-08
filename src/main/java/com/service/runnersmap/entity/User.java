75171a84694365632cf3b06000b0b6e782032fb2
lauren5141935 <lauren5141935@gmail.com>
2024-10-08 19:50:43 +0900

    feat: 컬럼 추가

--- src/main/java/com/service/runnersmap/entity/User.java
+++ src/main/java/com/service/runnersmap/entity/User.java
@@ -27,6 +27,9 @@ public class User extends BaseTimeEntity {
   @Column(name = "gender", nullable = false)
   private String gender;
 
+  @Column(name = "custom_time", nullable = false)
+  private String customTime;
+
   @Column(name = "provider", nullable = false)
   private String provider;
 
