7d10495325c1919cc33ceb7ed35155c60f6f722a
jeonjiwon <jiwon9609@naver.com>
2024-10-09 21:22:46 +0900

    refactor. 엘라스틱 설정 관련 삭제 (아직 미구현)

--- src/main/java/com/service/runnersmap/repository/UserPostRepository.java
+++ src/main/java/com/service/runnersmap/repository/UserPostRepository.java
@@ -1,11 +1,19 @@
 package com.service.runnersmap.repository;
 
+import com.service.runnersmap.entity.Post;
 import com.service.runnersmap.entity.UserPost;
 import com.service.runnersmap.entity.UserPostPK;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserPostRepository extends JpaRepository<UserPost, UserPostPK> {
+
+  @Query("SELECT u FROM UserPost u WHERE u.id.postId.id = :postId AND valid_yn = 1")
+  List<UserPost> findByPostId(@Param("postId") Long postId);
 
 }
