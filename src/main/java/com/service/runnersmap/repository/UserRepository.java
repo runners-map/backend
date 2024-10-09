a7d8ee6115f121f9052c751494d8959c264200db
lauren5141935 <lauren5141935@gmail.com>
2024-10-09 22:24:26 +0900

    chore: 패키지 위치 변경

--- src/main/java/com/service/runnersmap/repository/UserRepository.java
+++ src/main/java/com/service/runnersmap/repository/UserRepository.java
@@ -1,9 +1,11 @@
-package com.service.runnersmap;
+package com.service.runnersmap.repository;
 
 import com.service.runnersmap.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
   Optional<User> findByEmail(String email);
+
 }
