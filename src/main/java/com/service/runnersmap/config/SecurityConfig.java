ba04060c0947d6a04d2f5d8898d4a39da2b10aed
jeonjiwon <jiwon9609@naver.com>
2024-10-13 20:19:47 +0900

    feat : securityconfig 접근권한, EnableJpaAuditing 설정 추가(테이블createdts, updatedts 관련)

--- src/main/java/com/service/runnersmap/config/SecurityConfig.java
+++ src/main/java/com/service/runnersmap/config/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +26,8 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
     http
         .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
-        //HTTP 요청에 대한 권한
+        .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
+        // HTTP 요청에 대한 권한
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(
                 "api/user/sign-up",
@@ -31,12 +35,34 @@ public class SecurityConfig {
             ).permitAll()
             .anyRequest().authenticated()
         )
-        //  폼 로그인 기능을 비활성화
+        // 폼 로그인 기능을 비활성화
         .formLogin(form -> form.disable())
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // jwt 필터 추가
-
 
     return http.build();
   }
 
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true); // 쿠키 및 인증 정보를 허용
+    config.addAllowedOriginPattern("*"); // 모든 도메인을 허용 (cors관련)
+    config.addAllowedHeader("*"); // 모든 헤더를 허용
+    config.addAllowedMethod("*"); // 모든 HTTP 메서드를 허용
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+
+  private UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowCredentials(true); // 인증 관련 설정
+    configuration.addAllowedOriginPattern("*"); // 모든 도메인을 허용 (cors관련)
+    configuration.addAllowedHeader("*"); // 모든 헤더 허용
+    configuration.addAllowedMethod("*"); // 모든 메서드 허용 (GET, POST, PUT, DELETE 등)
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+    return source;
+  }
 }
