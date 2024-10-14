c98ee6f31c28614081ffbe73bfcf27b19baf4bd1
HueJo <slack_water@naver.com>
2024-10-14 20:51:46 +0900

    Feat : 댓글 작성, 수정, 삭제, 조회
    로그인 된 상태, 모집에 참여한 상태의 사람들만 사용할 수 있는 기능입니다.
    댓글 수정, 삭제는 작성자만 할 수 있습니다.
    댓글 조회는 페이징처리하여 사이즈는 20으로 처리해두었습니다.

---
+++ src/main/java/com/service/runnersmap/controller/CommentController.java
@@ -0,0 +1,97 @@
+package com.service.runnersmap.controller;
+
+import com.service.runnersmap.dto.CommentDto;
+import com.service.runnersmap.entity.User;
+import com.service.runnersmap.exception.RunnersMapException;
+import com.service.runnersmap.repository.UserRepository;
+import com.service.runnersmap.service.CommentService;
+import com.service.runnersmap.type.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+  private final CommentService commentService;
+  private final UserRepository userRepository;
+
+  // 댓글 작성
+  @PostMapping("/{postId}")
+  public ResponseEntity<Void> createComment(
+      @AuthenticationPrincipal UserDetails userDetails,
+      @PathVariable Long postId,
+      @RequestBody CommentDto commentDto)  {
+
+    User user = getCurrentUser(userDetails);
+
+    commentService.createComment(postId, user.getId(), commentDto);
+    return ResponseEntity.status((HttpStatus.CREATED)).build(); //201 Created
+  }
+
+
+  // 댓글 수정
+  @PatchMapping("/{commentId}")
+  public ResponseEntity<Void> updateComment(
+      @AuthenticationPrincipal UserDetails userDetails,
+      @PathVariable Long commentId,
+      @RequestBody CommentDto commentDto) {
+
+    User user = getCurrentUser(userDetails);
+
+    commentService.updateComment(commentId, user.getId(), commentDto);
+    return ResponseEntity.ok().build(); // 200 OK
+  }
+
+
+  // 댓글 삭제
+  @DeleteMapping("/{commentId}")
+  public ResponseEntity<Void> deleteComment(
+      @PathVariable Long commentId,
+      @AuthenticationPrincipal UserDetails userDetails) {
+
+    User user = getCurrentUser(userDetails);
+
+    commentService.deleteComment(commentId, user.getId());
+    return ResponseEntity.noContent().build(); // 204 No Content
+  }
+
+
+  // 댓글 조회
+  @GetMapping("/{postId}")
+  public ResponseEntity<Page<CommentDto>> getComments(
+      @PathVariable Long postId,
+      @AuthenticationPrincipal UserDetails userDetails,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "20") int size) {
+
+    User user = getCurrentUser(userDetails);
+
+    Page<CommentDto> commentDtos = commentService.getComments(postId, user.getId(),
+        PageRequest.of(page, size));
+    return ResponseEntity.ok(commentDtos);  // 200 OK
+  }
+
+  // 사용자 정보 조회
+  private User getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
+    String email = userDetails.getUsername();
+    return userRepository.findByEmail(email)
+        .orElseThrow(() -> new RunnersMapException(ErrorCode.NOT_FOUND_USER));
+  }
+}
+
---
+++ src/main/java/com/service/runnersmap/dto/CommentDto.java
@@ -0,0 +1,17 @@
+package com.service.runnersmap.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentDto {
+
+  private String nickname;
+  private String content;
+}
