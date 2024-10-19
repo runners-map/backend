ec3c25d0d7da275d347f876cf07dd6db69127fd3
HueJo <slack_water@naver.com>
2024-10-19 16:28:31 +0900

    충돌 수정중

--- src/main/java/com/service/runnersmap/controller/UserController.java
+++ src/main/java/com/service/runnersmap/controller/UserController.java
@@ -7,7 +7,6 @@ import com.service.runnersmap.dto.UserDto.AccountUpdateDto;
 import com.service.runnersmap.dto.UserDto.LoginDto;
 import com.service.runnersmap.dto.UserDto.SignUpDto;
 import com.service.runnersmap.service.UserService;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +16,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/user")
@@ -91,5 +87,5 @@ public class UserController {
     return ResponseEntity.ok().build(); // 200 OK
 
   }
-  
+
 }
