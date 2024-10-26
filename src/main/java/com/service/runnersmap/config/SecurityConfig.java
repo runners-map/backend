4a4f375b7ad9a14956bb8da376e56c389767ef2c
HueJo <slack_water@naver.com>
2024-10-26 19:11:45 +0900

    fix : 충돌 수정중

--- src/main/java/com/service/runnersmap/config/SecurityConfig.java
+++ src/main/java/com/service/runnersmap/config/SecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.web.filter.CorsFilter;
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Value("${spring.web.cors.allowed-origin}")
+  private String allowedOrigin;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -46,22 +48,11 @@ public class SecurityConfig {
     return http.build();
   }
 
-  @Bean
-  public CorsFilter corsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true); // 쿠키 및 인증 정보를 허용
-    config.addAllowedOriginPattern("*"); // 모든 도메인을 허용 (cors관련)
-    config.addAllowedHeader("*"); // 모든 헤더를 허용
-    config.addAllowedMethod("*"); // 모든 HTTP 메서드를 허용
-    source.registerCorsConfiguration("/**", config);
-    return new CorsFilter(source);
-  }
 
   private UrlBasedCorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowCredentials(true); // 인증 관련 설정
-    configuration.addAllowedOriginPattern("*"); // 모든 도메인을 허용 (cors관련)
+    configuration.addAllowedOriginPattern(allowedOrigin);
     configuration.addAllowedHeader("*"); // 모든 헤더 허용
     configuration.addAllowedMethod("*"); // 모든 메서드 허용 (GET, POST, PUT, DELETE 등)
 
